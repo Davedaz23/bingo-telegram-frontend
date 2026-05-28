7f3e3f81c5c46cd91fe97f7158487294bb5d1a7b
Davedaz23 <davegbhonesty@gmail.com>
2026-05-28 09:09:12 +0300

    game statis

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
     extend: {
       colors: {
         telegram: {
-          bg: 'var(--tg-theme-bg-color, #ffffff)',
+          bg: 'var(--tg-theme-bg-color, #34a6f1)',
           text: 'var(--tg-theme-text-color, #000000)',
-          hint: 'var(--tg-theme-hint-color, #999999)',
-          link: 'var(--tg-theme-link-color, #2481cc)',
+          hint: 'var(--tg-theme-hint-color, #80da64)',
+          link: 'var(--tg-theme-link-color, #44f84d)',
           button: 'var(--tg-theme-button-color, #2481cc)',
           buttonText: 'var(--tg-theme-button-text-color, #ffffff)',
           secondaryBg: 'var(--tg-theme-secondary-bg-color, #f0f0f0)',
